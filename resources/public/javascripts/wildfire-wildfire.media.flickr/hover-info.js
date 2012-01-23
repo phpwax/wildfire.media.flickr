1c1ee1caa7dca285cfbc889ff4e66a4443440e93
charlesmarshall <cm56marshall@googlemail.com>
2012-01-23 10:41:25 +0000

    the js preview for flickr files

---
+++ resources/public/javascripts/wildfire-wildfire.media.flickr/hover-info.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function(){
+
+  jQuery(window).bind("media.wildfireflickrfile.preview", function(e, row, preview_container){
+    var str = "";
+
+    row.find("td").each(function(){
+      var html = jQuery(this).html();
+
+      if(html.indexOf("<object") >= 0){
+        var h = parseInt(jQuery(html).find("object").attr("height")),
+            w = parseInt(jQuery(html).find("object").attr("width")),
+            r = 200/w
+            ;
+        console.log(w);
+        if(h && w) str += html.replace(h, Math.round(h*r)).replace(h, Math.round(h*r)).replace('"'+w+'"',200).replace('"'+w+'"',200);
+      }else if(html.indexOf("<img")) str += html.replace("_s.", "_m.").replace('width="40"', "");
+      else str += html;
+    });
+    preview_container.html(str);
+
+  });
+
+});
