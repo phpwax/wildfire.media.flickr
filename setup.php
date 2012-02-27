d82873ead20267e2ee53c71c5da860bdedce7251
charles marshall <charles@oneblackbear.com>
2012-02-27 14:44:44 +0000

    hidden include for cms

--- setup.php
+++ setup.php
@@ -1,3 +1,4 @@
 <?
+CMSApplication::register_module("media.flickr", array("hidden"=>true, "plugin_name"=>"wildfire.media.flickr", 'assets_for_cms'=>true));
 WildfireMedia::$classes[] = 'WildfireFlickrFile';
 ?>
